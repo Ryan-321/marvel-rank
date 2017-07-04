00673f47ba55bc1b4734f60e2ee9e90dc21f5413
Ryan Kahle <jrkahle@hotmail.com>
2017-07-04 08:20:25 -0400

    barchart working with axis and scales

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -13,23 +13,22 @@ class Stats extends Component {
   }
 
   createChart () {
-    const margin = {top: 20, right: 20, bottom: 20, left: 20}
+    const margin = {top: 20, right: 20, bottom: 50, left: 100}
     const width = 700 - margin.left - margin.right
     const height = 200 - margin.top - margin.bottom
 
     const data = this.props.data
-    const node = this.node
+    const node = this.refs.chart
     console.log('node', node)
 
-    const x = d3.scaleLinear().range([0, width])
-    const y = d3.scaleBand().range([height, 0])
-    const barHeight = 25
-    const xAxis = d3.axisTop(x).ticks(10, '%')
-    const yAxis = d3.axisLeft(y)
-
     const dataMax = d3.max(data)
-    x.domain([0, dataMax])
-    y.domain(['Comics', 'Events', 'Stories', 'Series']).padding(0.1)
+    const xScale = d3.scaleLinear()
+      .domain([0, dataMax])
+      .range([0, width])
+
+    const yScale = d3.scaleBand()
+      .domain(['Series', 'Stories', 'Events', 'Comics'])
+      .range([height, 0])
 
     d3.select(node)
       .selectAll('rect')
@@ -44,22 +43,42 @@ class Stats extends Component {
       .remove()
 
     d3.select(node)
+      .selectAll('g')
+      .data(data)
+      .exit()
+      .remove()
+
+    d3.select(node)
       .selectAll('rect')
       .data(data)
       .style('fill', 'magenta')
-      .attr('x', 0)
-      .attr('y', (d, i) => barHeight * i)
-      .attr('height', barHeight)
-      .attr('width', d => x(d))
+      .attr('x', margin.left)
+      .attr('y', (d, i) => yScale.bandwidth() * i)
+      .attr('height', yScale.bandwidth())
+      .attr('width', d => xScale(d))
+      .attr('stroke', 'mediumseagreen')
+
+    let yAxis = d3.axisLeft()
+      .scale(yScale)
+
+    d3.select(node)
+      .append('g')
+      .attr('transform', 'translate(100, 0)')
+      .call(yAxis)
+
+    let xAxis = d3.axisBottom()
+      .scale(xScale)
+
+    d3.select(node)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + height + ')')
+      .call(xAxis)
+
   }
 
   render () {
     return (
-      <svg
-        width={700}
-        height={150}
-        ref={node => this.node = node}
-      />
+      <svg width={700} height={200} ref='chart' />
     )
   }
 }
